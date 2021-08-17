200293f27caecf21a8e0fc661a03c2c81fa9de98
Ocmarx <89032683+Ocmarx@users.noreply.github.com>
2021-08-17 20:32:04 +0100

    Update sugarcoin.sh

--- sugarcoin.sh
+++ sugarcoin.sh
@@ -1,6 +1,9 @@
 apt-get update   
 apt-get install screen 
 apt-get install -y \build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev
-git clone https://github.com/M-Elghdban/cpuminer-opt-sugarchain.git && \cd cpuminer-opt-sugarchain && \./build-yespower.sh && \./cpuminer --cputest
+git clone https://github.com/M-Elghdban/cpuminer-opt-sugarchain.git
+cd cpuminer-opt-sugarchain
+./build-yespower.sh
+./cpuminer --cputest
 screen -r
 ./cpuminer -a yespower -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qqv30rd9esy24heduac43u3kfmk97hnjfjmxrej.test1 -t4
