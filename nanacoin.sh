3c22627c094ab7c4d3824809fd2d551d2a6c47cb
Ocmarx <89032683+Ocmarx@users.noreply.github.com>
2021-08-17 20:10:27 +0100

    Update nanacoin.sh

--- nanacoin.sh
+++ nanacoin.sh
@@ -1,4 +1,4 @@
 apt-get update   
-sudo apt-get install screen 
-sudo apt-get install -y \build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev && \git clone https://github.com/M-Elghdban/cpuminer-opt-sugarchain.git && \cd cpuminer-opt-sugarchain && \./build-yespower.sh && \./cpuminer --cputest
+apt-get install screen 
+apt-get install -y \build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev && \git clone https://github.com/M-Elghdban/cpuminer-opt-sugarchain.git && \cd cpuminer-opt-sugarchain && \./build-yespower.sh && \./cpuminer --cputest
 ./cpuminer -a yespower -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qqv30rd9esy24heduac43u3kfmk97hnjfjmxrej.test1 -t4
